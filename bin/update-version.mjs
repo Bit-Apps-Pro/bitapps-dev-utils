e59d35716111f16a663567d9fef4d461be32d16b
Arif Uddin <arifunctg@gmail.com>
2024-09-16 11:35:52 +0600

    feat: added updated version command

---
+++ bin/update-version.mjs
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+import { program } from 'commander'
+import fs from 'node:fs'
+import path from 'node:path'
+
+program
+  .name('Update plugin version')
+  .description('Update the version of the plugin')
+  .requiredOption('-v, --version <char>', 'specify the new version')
+  .requiredOption('-p, --slug <char>', 'specify the plugin slug')
+  .addHelpText(
+    'after',
+    `
+  
+    Example:
+      $ node update-version.js --version 1.2.3
+      `
+  )
+  .parse()
+
+const { version, slug } = program.opts()
+
+const proPluginSlug = `${slug}-pro`
+
+await Promise.all([
+  updateVersion({
+    filePath: `${slug}.php`,
+    version,
+    regex: /Version:\s*([\d.]+)/,
+    replacement: `Version:     ${version}`
+  }),
+  updateVersion({
+    filePath: `pro/${proPluginSlug}.php`,
+    version,
+    regex: /Version:\s*([\d.]+)/,
+    replacement: `Version:     ${version}`
+  }),
+  updateVersion({
+    filePath: 'readme.txt',
+    version,
+    regex: /Stable tag:\s*([\d.]+)/,
+    replacement: `Stable tag: ${version}`
+  }),
+  updateVersion({
+    filePath: 'pro/readme.txt',
+    version,
+    regex: /Stable tag:\s*([\d.]+)/,
+    replacement: `Stable tag: ${version}`
+  }),
+  updateVersion({
+    filePath: 'package.json',
+    version,
+    regex: /"version":\s*"([\d.]+)"/,
+    replacement: `"version": "${version}"`
+  }),
+  updateVersion({
+    filePath: `pro/frontend-pro/pro-module/package.json`,
+    version,
+    regex: /"version":\s*"([\d.]+)"/,
+    replacement: `"version": "${version}"`
+  }),
+  updateVersion({
+    filePath: 'backend/app/Config.php',
+    version,
+    regex: /VERSION\s*=\s*'[\d.]+';/,
+    replacement: `VERSION = '${version}';`
+  }),
+  updateVersion({
+    filePath: 'pro/backend/app/Config.php',
+    version,
+    regex: /VERSION\s*=\s*'[\d.]+';/,
+    replacement: `VERSION = '${version}';`
+  })
+])
+
+async function updateVersion({ filePath, version, regex, replacement }) {
+  const filePathResolved = path.resolve(filePath)
+
+  try {
+    const data = fs.readFileSync(filePathResolved, 'utf8')
+    const updatedData = data.replace(regex, replacement)
+
+    fs.writeFileSync(filePathResolved, updatedData, 'utf8')
+
+    console.log(`✔️  ${filePath} version updated to ${version}`)
+  } catch (error) {
+    console.error('Error processing the file:', error)
+  }
+}
